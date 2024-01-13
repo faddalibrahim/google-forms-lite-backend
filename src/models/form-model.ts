949c5c765bc9cefc4cfa68d787381bff732a162a
Faddal Ibrahim <faddalibrahim@gmail.com>
2024-01-13 11:32:28 +0000

    feat: add name field to Form schema

--- src/models/form-model.ts
+++ src/models/form-model.ts
@@ -1,41 +1,20 @@
 import mongoose, { Schema, Types, Document } from "mongoose";
-const { ObjectId } = Types;
 
 interface IForm extends Document {
-  _userId: { type: typeof ObjectId; required: boolean };
+  _userId: { type: typeof Types.ObjectId; required: boolean };
+  name: string;
   isPublished: boolean;
   isLocked: boolean;
   createdAt: Date;
 }
 
 const formSchema = new Schema<IForm>({
-  _userId: { type: ObjectId, required: true },
+  _userId: { type: Types.ObjectId, required: true },
+  name: { type: String, required: true },
   isPublished: { type: Boolean, default: false },
   isLocked: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now },
 });
-
-// const Form = model<IForm>("Form", formSchema);
-
-// interface IPage {
-//   id: string;
-//   title: string;
-//   subtitle: string;
-//   questions: IQuestion[];
-// }
-
-// interface IQuestion {
-//   id: string;
-//   question: string;
-//   type: string; // radio, checbox, text, textarea, date etc
-//   required: boolean;
-//   options?: IOption[]; // only for radio and checkbox
-// }
-
-// interface IOption {
-//   id: string;
-//   option: string;
-// }
 
 const Form = mongoose.model<IForm>("Form", formSchema);
 
