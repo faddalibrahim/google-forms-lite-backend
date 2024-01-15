1d7be1de91e3b85d1e4fbab9558c7326fa325b7a
Faddal Ibrahim <faddalibrahim@gmail.com>
2024-01-15 11:49:11 +0000

    feat: include minLength in password and unique true in username for the user schema

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -4,8 +4,8 @@ import mongoose, { Schema, Document } from "mongoose";
 // Define a schema
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true, lowercase: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true, lowercase: true },
+  password: { type: String, required: true, minLength: 8 },
   salt: { type: String, required: true },
 });
 
