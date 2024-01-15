9981c393a2fa1b50e323ed35c43828c6b530f314
Faddal Ibrahim <faddalibrahim@gmail.com>
2024-01-15 12:00:56 +0000

    feat: add validation to user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,11 +1,28 @@
 // Import necessary modules and types
 import mongoose, { Schema, Document } from "mongoose";
+import { emailInvalid } from "../utils/validation.util";
 
 // Define a schema
 const userSchema = new Schema({
-  username: { type: String, required: true, unique: true, lowercase: true },
-  email: { type: String, required: true, unique: true, lowercase: true },
-  password: { type: String, required: true, minLength: 8 },
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    unique: true,
+    lowercase: true,
+    minlength: [6, "Username must be at least 4 characters long"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    lowercase: true,
+    validate: [(email: string) => !emailInvalid(email), "Email is invalid"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [8, "Password must be at least 8 characters long"],
+  },
   salt: { type: String, required: true },
 });
 
