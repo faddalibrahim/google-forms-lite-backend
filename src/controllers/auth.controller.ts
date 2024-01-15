00c090036869e5ce73fc81d53bbad69f00dedbf0
Faddal Ibrahim <faddalibrahim@gmail.com>
2024-01-15 11:34:12 +0000

    feat: use user validation functions in auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,32 +1,49 @@
 import { Request, Response } from "express";
 import * as AuthService from "../services/auth.service";
+import {
+  hasMissingFields,
+  passwordsDontMatch,
+  passwordInvalid,
+  emailInvalid,
+} from "../utils/validation.util";
 
 export const register = async (req: Request, res: Response) => {
   const { username, email, password, repeatPassword } = req.body;
 
-  // missing fields
-  if (!username || !email || !password || !repeatPassword) {
+  if (
+    hasMissingFields(req.body, [
+      "username",
+      "email",
+      "password",
+      "repeatPassword",
+    ])
+  ) {
     return res
       .status(400)
       .json({ successful: false, message: "Missing required fields" });
   }
 
-  // password length, passwords match, contains required characters
-  if (password.length < 6) {
-    return res
-      .status(400)
-      .json({
-        successful: false,
-        message: "Password must be at least 6 characters",
-      });
+  if (passwordInvalid(password, 8)) {
+    return res.status(400).json({
+      successful: false,
+      message:
+        "Password must be at least 8 characters and contain at least one uppercase letter, one lowercase letter, one number, and one special character",
+    });
   }
 
-  // email validation
-  if (!email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
+  if (passwordsDontMatch(password, repeatPassword)) {
+    return res
+      .status(400)
+      .json({ successful: false, message: "Passwords do not match" });
+  }
+
+  if (emailInvalid(email)) {
     return res
       .status(400)
       .json({ successful: false, message: "Invalid email address" });
   }
+
+  return res.json({ successful: true, message: "validation successful" });
 
   try {
     await AuthService.registerUser({ username, email, password });
