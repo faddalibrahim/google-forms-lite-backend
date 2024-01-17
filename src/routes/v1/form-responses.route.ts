2b4e3dc57b673a8adb1eb82f5e8b7407fb6ef004
Faddal Ibrahim <faddalibrahim@gmail.com>
2024-01-17 10:33:38 +0000

    feat: add requireAuth middle ware to form responses routes

--- src/routes/v1/form-responses.route.ts
+++ src/routes/v1/form-responses.route.ts
@@ -1,7 +1,10 @@
 import express, { Router, Request, Response } from "express";
 import * as formResponsesController from "../../controllers/form-response.controller";
+import requireAuth from "../../middlewares/require-auth.middleware";
 
 const router: Router = express.Router();
+
+router.use(requireAuth);
 
 router.get("/", formResponsesController.getAllFormResponses);
 router.get("/:id", formResponsesController.getFormResponse);
