806318bc5e458a57f3b5fa59c5e32fc0d7218f64
Faddal Ibrahim <faddalibrahim@gmail.com>
2024-01-17 10:31:44 +0000

    feat: change logout to get request

--- src/routes/v1/auth.route.ts
+++ src/routes/v1/auth.route.ts
@@ -5,6 +5,6 @@ const router: Router = express.Router();
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
-router.post("/logout", authController.logout);
+router.get("/logout", authController.logout);
 
 export default router;
