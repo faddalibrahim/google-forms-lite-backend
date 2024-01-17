865b5df0be22a299dd56ae674d4e46cf99c9eace
Faddal Ibrahim <faddalibrahim@gmail.com>
2024-01-17 10:04:55 +0000

    feat: add logout to auth route

--- src/routes/v1/auth.route.ts
+++ src/routes/v1/auth.route.ts
@@ -5,5 +5,6 @@ const router: Router = express.Router();
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
+router.post("/logout", authController.logout);
 
 export default router;
